a78b8363a2bef533d8d5ffb53ea29229b49dacfe
onuriltan <onur.iltan@gmail.com>
2019-04-30 10:50:10 +0300

    first problem added

---
+++ index.js
@@ -0,0 +1,14 @@
+function elevatorSupertition(input) {
+  let luckyArray = [];
+  let tempInput = input;
+  for(let j  = 1 ; j<=tempInput ; j++) {
+    luckyArray.push(j);
+    if(j.toString().indexOf('4') > -1 || j.toString().indexOf('13') > -1 ) {
+      luckyArray.pop();
+      tempInput ++;
+    }
+  }
+  console.log(luckyArray[input-1]);
+}
+
+//elevatorSupertition(12);
