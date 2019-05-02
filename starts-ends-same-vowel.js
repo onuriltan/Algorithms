bceb1601649f194e7f983965bbcaf4ab2b60be56
onuriltan <onur.iltan@gmail.com>
2019-05-02 10:55:29 +0300

    add

--- starts-ends-same-vowel.js
+++ starts-ends-same-vowel.js
@@ -1,4 +1,4 @@
-
+// https://www.hackerrank.com/challenges/js10-regexp-1/problem
 function regexVar(text) {
     let re = text.match(/^([aeiou]).*\1$/i);
     re === null ? console.log("false") : console.log("true");
