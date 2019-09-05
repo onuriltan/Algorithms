c7375a3e2a94879d7c36ef8e6150c64f8b4ad05b
onuriltan <onur.iltan@gmail.com>
2019-09-05 10:31:30 +0300

    fizzbuzz better fix

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,9 +1,8 @@
 function fizzbuzz(number) {
-    let result = ''
-    if(number % 3 === 0) result = result + 'Fizz'
-    if(number % 5 === 0) result = result + 'Buzz'
-    else result = number
-    console.log(result)
+    if(number % 15 === 0) console.log('FizzBuzz')
+    else if(number % 3 === 0) console.log('Fizz')
+    else if(number % 5 === 0) console.log('Buzz')
+    else console.log(number)
     if(number > 1) fizzbuzz(number - 1)
 }
 fizzbuzz(20)
